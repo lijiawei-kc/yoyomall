7294351557944a7c8c937a25322af06622b20050
liuxi <lx2284420573@163.com>
2022-08-21 12:23:12 +0800

    订单模块搭建

---
+++ src/main/java/com/yoyo/yoyomall/mapper/OrdersMapper.java
@@ -0,0 +1,15 @@
+package com.yoyo.yoyomall.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yoyo.yoyomall.entity.Orders;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author atguigu
+ * @since 2022-08-21
+ */
+public interface OrdersMapper extends BaseMapper<Orders> {
+}
